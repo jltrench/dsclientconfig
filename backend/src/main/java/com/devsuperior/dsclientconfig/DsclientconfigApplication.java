d470d98d5bf74a8ee65023b2196e35a6def7fd4c
João Lucca Trench <jltrench@gmail.com>
2021-09-20 07:36:32 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/dsclientconfig/DsclientconfigApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsclientconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsclientconfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsclientconfigApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dsclientconfig/DsclientconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsclientconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsclientconfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
